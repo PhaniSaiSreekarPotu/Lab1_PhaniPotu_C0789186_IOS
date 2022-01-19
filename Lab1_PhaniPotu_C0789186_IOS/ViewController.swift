97fe9b08a4d51e2c9b3b37c18f934b680885ed15
sreekar <sreekarch26@gmail.com>
2022-01-18 17:06:39 -0800

    Initial Commit

---
+++ Lab1_PhaniPotu_C0789186_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab1_PhaniPotu_C0789186_IOS
+//
+//  Created by Mac on 18/01/22.
+//  Copyright © 2022 sreekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
